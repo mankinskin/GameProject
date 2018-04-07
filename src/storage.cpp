83b0b8cfb11b434aa6387436ebed3d3692adadea
Linus Behrbohm <linus@debian.deb.linus.de>
2018-04-07 20:10:22 +0100

    Added Storage module (again)

--- src/storage.cpp
+++ src/storage.cpp
@@ -1,1 +1,32 @@
 #include "storage.h"
+#include <unordered_map>
+
+std::unordered_map<unsigned int, unsigned int> targetBindingCounts;
+int gl::SHADER_STORAGE_BUFFER_OFFSET_ALIGNMENT = 0;
+int gl::UNIFORM_BUFFER_OFFSET_ALIGNMENT = 0;
+int gl::MAX_UNIFORM_BUFFER_BINDINGS = 0;
+int gl::MIN_MAP_BUFFER_ALIGNMENT = 0;
+
+gl::Storage gl::createStorage( std::string pName, unsigned int pCapacity, unsigned int pFlags, void* pData )
+{
+	unsigned int id = 0;
+	glCreateBuffers( 1, &id );
+	glNamedBufferStorage( id, pCapacity, pData, pFlags );
+	return Storage( pName, id, pCapacity, pFlags );
+}
+
+unsigned int getNewTargetBinding( const unsigned int pTarget )
+{
+	auto target_it = targetBindingCounts.find( pTarget );
+	if ( target_it == targetBindingCounts.end() ) {
+		target_it = targetBindingCounts.find( pTarget );
+	}
+	return target_it->second++;
+}
+
+void gl::setStorageTarget( gl::Storage& pStorage, const unsigned int pTarget )
+{
+	pStorage.target = pTarget;
+	pStorage.binding = getNewTargetBinding( pStorage.target );
+	glBindBufferBase( pStorage.target, pStorage.binding, pStorage.ID );	
+}
--- src/storage.h
+++ src/storage.h
@@ -1,9 +1,30 @@
 #pragma once
+#include <string>
+#include <glew.h>
 
 namespace gl
 {
-	class Storage 
-	{
+	extern int SHADER_STORAGE_BUFFER_OFFSET_ALIGNMENT;
+	extern int UNIFORM_BUFFER_OFFSET_ALIGNMENT;
+	extern int MAX_UNIFORM_BUFFER_BINDINGS;
+	extern int MIN_MAP_BUFFER_ALIGNMENT;
+	const int MAP_PERSISTENT_FLAGS = GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT;
 
+	struct Storage 
+	{
+		Storage()
+		{}
+		Storage( std::string pName, unsigned int pID, unsigned int pCapacity, unsigned int pFlags)
+			:name( pName ), ID( pID ), capacity( pCapacity ), flags( pFlags )
+		{}
+		std::string name;
+		unsigned int ID;
+		unsigned int capacity;
+		unsigned int flags;
+		unsigned int target;
+		unsigned int binding;
 	};
+
+	Storage createStorage( std::string pName, unsigned int pCapacity, unsigned int pFlags, void* pData = nullptr );
+	void setStorageTarget( Storage& pStorage, const unsigned int pTarget );
 }
