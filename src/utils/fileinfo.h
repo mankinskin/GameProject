6feee30552752173beb16162a4be75250a4536bc
Linus Behrbohm <linusbehrbohm@web.de>
2018-06-10 21:48:04 +0200

    Add Fileinfo class
    Add a class for encapsulating a path and provide a few member functions to work with it.

---
+++ src/utils/fileinfo.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include "path.h"
+
+namespace path
+{
+	class Fileinfo
+	{
+		std::string path;
+
+		bool isAbsolute() const
+		{
+			return path::isAbsolute( path );
+		}
+		bool isRelative() const
+		{
+			return path::isRelative( path );
+		}
+
+		std::string extension() const
+		{
+			return path::getExtension( path );
+		}
+
+		std::string path() const
+		{
+			return path::getPath( path );
+		}
+
+		std::string name() const
+		{
+			return path::getFilename( path );
+		}
+	};
+}
