fa54d4c38b24bc8251953b38b9bb8e6c68f20cef
Linus Behrbohm <linusbehrbohm@web.de>
2018-06-19 11:31:15 +0200

    Rename utils::gen_Element_t to gen_tuple_t

--- src/element_utils.h
+++ src/element_utils.h
@@ -10,18 +10,17 @@ namespace utils
     template<typename T> struct _type { using type = T; };
 
     template<unsigned int N, typename Type, typename ...SubElements>
-        struct element_generator 
+        struct tuple_generator 
         {
-            using type = typename element_generator<N - 1, Type, Type, SubElements...>::type;
+            using type = typename tuple_generator<N - 1, Type, Type, SubElements...>::type;
         };
     template<typename Type, typename ...SubElements>
-        struct element_generator<0, Type, SubElements...> 
+        struct tuple_generator<0, Type, SubElements...> 
         {
-            using type = Element<SubElements...>;
+            using type = std::tuple<SubElements...>;
         };
 
     template<typename Type, unsigned int Count>
-        using gen_Element_t = typename element_generator<Count, Type>::type;
-
+        using gen_tuple_t = typename tuple_generator<Count, Type>::type;
 }    
 
