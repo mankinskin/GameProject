47968bfe3c36bacf03442c14431542da8d6f8984
Linus Behrbohm <linusbehrbohm@web.de>
2018-06-27 19:52:40 +0200

    Rename Event to Listener and make it store its state
    Listeners are a family of classes which each listen for some type of
    event. Each of their instances holds an event signature and sets its
    occurred field to true if the signature occurred in its eventBuffer.
    the status can later be gotten through the stat() function.

--- src/event.cpp
+++ src/event.cpp
@@ -1,7 +1,9 @@
 #include "event.h"
 
 std::vector<void(*)()> events::checkers = std::vector<void(*)()>();
+std::vector<bool(*)(size_t)> events::staters = std::vector<bool(*)(size_t)>();
 std::vector<void(*)()> events::resetters = std::vector<void(*)()>();
+size_t events::template_count = 0;
 
 void events::checkEvents() 
 {
--- src/event.h
+++ src/event.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "signal.h"
 #include "gates.h"
 #include <vector>
 
@@ -11,72 +10,119 @@ namespace events
     void checkEvents();
     void resetEvents();
 
-    extern std::vector<void(*)()> checkers;  // checks all Event queues
+    extern size_t template_count;
+    extern std::vector<void(*)()> checkers;  // for checking all Events
+    extern std::vector<bool(*)(size_t)> staters;  // for getting the status of all listeners
     extern std::vector<void(*)()> resetters; // resets all Event queues
 
-    template<typename T>
-        class Event
+    template<typename Event>
+        class Listener
         {
             public:
-                using ID = utils::ID<Event<T>>;
-                static constexpr typename ID::Container& all = ID::container;
+                static std::vector<Listener<Event>> all;
 
-                Event( T&& pSignature, signals::Signal::ID pSignal = signals::Signal::ID() )
+                Listener( const Event pSignature )
                     : signature( pSignature )
-                    , signal( pSignal )
-                {}
-
-                static void clearQueue()
+                    , occurred( false )
                 {
-                    queue.clear();
+                    (void)init;
                 }
 
-                // throw signal if sig matches
-                void check( const T& sig )
+                static void push( Event pEvent )
                 {
-                    if ( sig == signature ) {
-                        signal->state = true;
+                    if( all.size() ) {  // if listening for any events
+                        eventBuffer.push_back( pEvent );
                     }
                 }
 
-                // throws the signals of the events in queue
-                static void checkQueue()
+                static void clearBuffer()
                 {
-                    for ( size_t q = 0; q < queue.size(); ++q ) {
-                        for ( size_t e = 0; e < all.size(); ++e ) {
-                            all[e].check( queue[q] );
+                    eventBuffer.clear();
+                }
+
+                void checkEvent( Event pEvent )
+                {
+                    if ( !occurred && pEvent == signature ) {
+                        occurred = true;
+                    }
+                }
+
+                static void checkAll()
+                {
+                    for( Listener<Event>& instance : all ) {
+                        instance.occurred = false;
+                        for( const Event& event : eventBuffer ) {
+                            instance.checkEvent( event );
                         }
                     }
+                    clearBuffer();
                 }
 
-                const signals::Signal::ID signal;
-                T signature;
-                static std::vector<T> queue;
+                static bool stat( size_t i )
+                {
+                    return all[i].occurred;
+                }
+
+                const Event signature;
+                bool occurred = false;
+
+                static std::vector<Event> eventBuffer;
             private:
+                friend struct ListenerID;
+                static size_t templateIndex;  // the index of the template instantiation
                 struct At_Init
                 {
                     At_Init()
                     {
-                        checkers.push_back( checkQueue );
-                        resetters.push_back( clearQueue ); 
+                        templateIndex = template_count++;
+                        checkers.push_back( checkAll );
+                        staters.push_back( stat );
+                        resetters.push_back( clearBuffer );
                     }
                 };
                 static At_Init init;
         };
 
-    template<typename T>
-        std::vector<T> Event<T>::queue = std::vector<T>();
-    template<typename T>
-        typename Event<T>::At_Init Event<T>::init = Event<T>::At_Init();
+    template<typename Event>
+        std::vector<Listener<Event>> Listener<Event>::all = std::vector<Listener<Event>>();
 
-    template<typename T>
-        typename Event<T>::ID createEvent( Event<T> pEvent )
+    template<typename Event>
+        std::vector<Event> Listener<Event>::eventBuffer = std::vector<Event>();
+
+    template<typename Event>
+        size_t Listener<Event>::templateIndex = 0;
+
+    template<typename Event>
+        typename Listener<Event>::At_Init Listener<Event>::init = Listener<Event>::At_Init();
+
+    template<typename Event>
+        void pushEvent( Event pEvent )
         {
-            return Event<T>::ID( pEvent );
+            Listener<Event>::push( pEvent );
         }
-    template<typename T>
-        void pushEvent( T pEvent )
+
+    struct ListenerID
+    {
+        template<typename Event>
+            ListenerID( const Listener<Event> pListener )
+            : templateIndex( Listener<Event>::templateIndex )
+            , instanceIndex( Listener<Event>::all.size() )
         {
-            Event<T>::queue.push_back( pEvent );
+            Listener<Event>::all.push_back( pListener );
+        }
+
+        bool stat() const
+        {
+            return staters[templateIndex]( instanceIndex );
+        }
+        private:
+        size_t templateIndex;
+        size_t instanceIndex;
+    };
+
+    template<typename Event>
+        ListenerID listenForEvent( Event pEvent )
+        {
+            return ListenerID( Listener<Event>( pEvent ) );
         }
 }
