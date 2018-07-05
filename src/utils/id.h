b8a3525463f723ac4e6ff4389ddf8cece2ac0574
Linus Behrbohm <linusbehrbohm@web.de>
2018-07-05 22:56:42 +0200

    Make ID reference safe
    ID can now safely be used with a reference type

--- src/utils/id.h
+++ src/utils/id.h
@@ -1,47 +1,58 @@
 #pragma once
 #include <vector>
 #include <functional>
+#include <type_traits>
+#include <assert.h>
 
 namespace utils
 {
+    const size_t INVALID_ID = -1;
+    template<typename T>
+    using ID_Container = typename std::vector<T>;
+
+    template<typename T>
+    using ID_Type = typename std::remove_reference<T>::type;
+
+    template<typename T>
+    ID_Container<T> id_container;
+
     template<typename T>
         struct ID
         {
-            using Container = typename std::vector<T>;
+            using Type = ID_Type<T>;
+            using Container = ID_Container<Type>;
             using size_type = typename Container::size_type;
-            static Container container;
+            constexpr static Container& container = id_container<Type>;
+
             ID()
-                :index( container.size() )
-            {
-                container.push_back( T() );
-            }
+                :index(INVALID_ID)
+            {}
 
             ID( const size_t i )
                 :index( i )
             {}
 
-            ID( const T obj )
+            ID( T obj )
                 :index( container.size() )
             {
                 container.push_back( obj );
             }
+
             size_type index;
 
-            T& get() const
+            Type& get() const
             {
+                assert( container.size() );
                 return container[index];
             }
 
-            T* operator->() const
+            Type* operator->() const
             {
                 return &get();
             }
-            T& operator*() const
+            Type& operator*() const
             {
                 return get();
             }
         };
-
-    template<typename T>
-        typename ID<T>::Container ID<T>::container = ID<T>::Container();
 }
