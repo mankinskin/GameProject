080e10137da2af0819943c1ea390a1e44b5601b9
Linus Behrbohm <linusbehrbohm@web.de>
2019-04-02 02:10:47 +0200

    Change textbox to display keybindings

--- src/textbox.cpp
+++ src/textbox.cpp
@@ -5,9 +5,9 @@ utils::Container<text::Textbox> text::Textbox::all = utils::Container<text::Text
 
 void text::setupTextboxes()
 {
-  Textbox::ID term_box1 = text::Textbox::all.makeID(text::Textbox(glm::vec2(-1.0f, 0.0f), glm::vec2(0.5f, 1.0f)));
+  text::Textbox::ID term_box1 = text::Textbox::all.makeID(text::Textbox(glm::vec2(-1.0f, 0.0f), glm::vec2(0.5f, 1.0f)));
   term_box1->getText() = "J - change camera\nC - Toggle cursor\nG - Toggle Coordinate System\nH - Toggle Axis";
-  Textbox::ID term_box2 = text::Textbox::all.makeID(text::Textbox(glm::vec2(0.0f, 0.0f), glm::vec2(0.2f, 0.5f)));
+  text::Textbox::ID term_box2 = text::Textbox::all.makeID(text::Textbox(glm::vec2(0.0f, 0.0f), glm::vec2(0.2f, 0.5f)));
   term_box2->getText() = "A textbox\n\nYo sup";
   //term_box2->setFont(1);
 }
