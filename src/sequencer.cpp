07e229490890c5cb42e2d1764baced99f4b5eb77
Linus Behrbohm <linusbehrbohm@web.de>
2019-04-02 02:10:47 +0200

    Change textbox to display keybindings

--- src/sequencer.cpp
+++ src/sequencer.cpp
@@ -159,7 +159,7 @@ void sequencer::gameloop()
   glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
   text::Textbox::ID term_box1 = text::Textbox::all.makeID(text::Textbox(glm::vec2(-1.0f, 0.0f), glm::vec2(0.5f, 1.0f)));
-  term_box1->getText() = "1.\tfirst item\n2.\tsecond item\n3.\tthird item\n...\n10.\ttenth item";
+  term_box1->getText() = "J - change camera\nC - Toggle cursor\nG - Toggle Coordinate System\nH - Toggle Axis";
   text::Textbox::ID term_box2 = text::Textbox::all.makeID(text::Textbox(glm::vec2(0.0f, 0.0f), glm::vec2(0.2f, 0.5f)));
   term_box2->getText() = "A textbox\n\nYo sup";
   //term_box2->setFont(1);
